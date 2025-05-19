c4ed1e3dfa98d90143f78e8e50f9cfb89f1e2f16
TZGyn <spectator_brethren761@slmails.com>
2025-05-19 17:00:28 +0800

    improved file paths sorter

--- src/lib/utils/files/path-sort.ts
+++ src/lib/utils/files/path-sort.ts
@@ -1,33 +1,57 @@
-// https://github.com/hughsk/path-sort
-export function pathsort(paths: string[], sep?: string) {
-	sep = sep || '/'
+// modified version of https://github.com/ghornich/sort-paths
+export function pathsort(items: string[]) {
+	return items
+		.map(function (item) {
+			const path = item
 
-	return paths
-		.map(function (el) {
-			return el.split(sep)
+			return {
+				item: item,
+				pathTokens: path.split('/').map((item, index) => {
+					if (index !== path.split('/').length - 1) {
+						return item + '/'
+					} else {
+						return item
+					}
+				}),
+			}
 		})
-		.sort(sorter)
-		.map(function (el) {
-			return el.join(sep)
+		.sort(function (item_a, item_b) {
+			const tokensA = item_a.pathTokens
+			const tokensB = item_b.pathTokens
+
+			for (
+				let i = 0, len = Math.max(tokensA.length, tokensB.length);
+				i < len;
+				i++
+			) {
+				if (!(i in tokensA)) {
+					return -1
+				}
+
+				if (!(i in tokensB)) {
+					return 1
+				}
+
+				const tokenA = tokensA[i].toLowerCase()
+				const tokenB = tokensB[i].toLowerCase()
+
+				if (tokenA === tokenB) {
+					continue
+				}
+
+				const isTokenADir = tokenA[tokenA.length - 1] === '/'
+				const isTokenBDir = tokenB[tokenB.length - 1] === '/'
+
+				if (isTokenADir === isTokenBDir) {
+					return tokenA < tokenB ? -1 : 1
+				} else {
+					return isTokenADir ? 1 : -1
+				}
+			}
+
+			return 0
 		})
-}
-
-function sorter(a: string[], b: string[]) {
-	var l = Math.max(a.length, b.length)
-	for (var i = 0; i < l; i += 1) {
-		if (!(i in a)) return -1
-		if (!(i in b)) return +1
-		if (a[i].toUpperCase() > b[i].toUpperCase()) return +1
-		if (a[i].toUpperCase() < b[i].toUpperCase()) return -1
-		if (a.length < b.length) return -1
-		if (a.length > b.length) return +1
-	}
-	return 0
-}
-
-export function standalone(sep: string) {
-	sep = sep || '/'
-	return function pathsort(a: string, b: string) {
-		return sorter(a.split(sep), b.split(sep))
-	}
+		.map(function (item) {
+			return item.item
+		})
 }
